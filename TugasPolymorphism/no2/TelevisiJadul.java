44dd0b52b4f4597ec88a7122e2ec6a5e375a9490
Kinanti Permata Putri <kinanti.permat@gmail.com>
2019-11-06 08:34:14 +0700

    jobsheet 10

---
+++ TugasPolymorphism/no2/TelevisiJadul.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TugasPolymorphism.no2;
+
+/**
+ *
+ * @author kinanpermata
+ */
+public class TelevisiJadul extends Elektronik{
+    private String modelInput;
+    
+    TelevisiJadul(){
+        
+    }
+
+    public String getModelInput() {
+        return modelInput;
+    }
+}
